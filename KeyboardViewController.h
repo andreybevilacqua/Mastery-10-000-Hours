ce0d65bbf393fb854db8e2e1fb5247106153c471
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2016-07-24 14:40:12 -0300

    Working in keyboard function.

--- KeyboardViewController.h
+++ KeyboardViewController.h
@@ -1,13 +1,13 @@
 //
-//  PlusTimeViewController.h
+//  KeyboardViewController.h
 //  Mastery 10,000 Hours
 //
-//  Created by Andrey Bevilacqua on 7/19/16.
+//  Created by Andrey Bevilacqua on 7/24/16.
 //  Copyright © 2016 Andrey Bevilacqua. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface PlusTimeViewController : UIViewController
+@interface KeyboardViewController : UIViewController
 
 @end
---
+++ KeyboardViewController.m
@@ -0,0 +1,29 @@
+//
+//  KeyboardViewController.m
+//  Mastery 10,000 Hours
+//
+//  Created by Andrey Bevilacqua on 7/24/16.
+//  Copyright © 2016 Andrey Bevilacqua. All rights reserved.
+//
+
+#import "KeyboardViewController.h"
+
+@interface KeyboardViewController ()
+
+@end
+
+@implementation KeyboardViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    
+    [super didReceiveMemoryWarning];
+    
+}
+
+@end
